0ee6613acca1f96ac9b46f467f5a8cf9e7464549
Rawiri Blundell <rawiri.blundell@gmail.com>
2021-08-05 08:15:22 +1200

    Update extract and passwordgen

--- archive/extract.sh
+++ archive/extract.sh
@@ -13,67 +13,91 @@
 #//  Modified: 24, July, 2021                                //
 #//  file: -                                                 //
 #//  -                                                       //
-#//  Source: https://www.quora.com/What-is-the-most-useful-bash-script-that-you-have-ever-written                                                //
+#//  Source: https://www.quora.com/What-is-the-most-useful-bash-script-that-you-have-ever-written //
 #//          https://wiki.archlinux.org/title/Archiving_and_compression
-#//          https://github.com/xvoland/Extract
+#//          https://github.com/xvoland/Extract              //
+#//          https://raw.githubusercontent.com/rawiriblundell/dotfiles/master/.bashrc //
 #//  OS: ALL                                                 //
 #//  CPU: ALL                                                //
 #//                                                          //
 #//////////////////////////////////////////////////////////////
 
-extractAllTypeFiles(){ 
-       if [ -f "$1" ] ; then 
-        case $1 in 
-           *.ark)       arc x "$1"            ;; 
-           *.arc)       arc x "$1"            ;; 
-           *.arj)       arj e "$1"            ;; 
-           *.cbt)       tar xvf "$1"          ;;
-           *.cso)       ciso 0 ./"$1" ./"$1.iso" && \
-                extract "$1".iso && \rm -f "$1" ;;
-           *.tar.bz2)   tar xvjf "$1"         ;; 
-           *.tar.gz)    tar xvzf "$1"         ;; 
-           *.tar.lzma)  tar --lzma -xvf "$1"  ;; 
-           *.tar.xz)    tar -xf "$1"          ;; 
-           *.tar.lz)    lzip -d "$1"          ;; #tar --lzip -tf $1   ;;
-           *.tar.7z)    7z x -so "$1" | tar -xf - ;; # tar -cJf
-           *.tar.Z)     zcat "$1" | tar -xvf - ;; 
-           *.jar)       jar xf "$1"           ;; 
-           *.bz2)       bunzip2 "$1"          ;; 
-           *.rar)       unrar x "$1"          ;;
-           *.cbr)       unrar x -ad "$1"      ;;
-           *.gz)        gunzip "$1"           ;; 
-           *.cab)       cabextract "$1"       ;;
-           *.cpio)      cpio -id < "$1"       ;;
-           *.cba|*.ace) unace x "$1"          ;;
-           *.tar)       tar xvf "$1"          ;; 
-           *.tbz2)      tar xvjf "$1"         ;; 
-           *.tgz)       tar xvzf "$1"         ;; 
-           *.lz4)       lz4 -d "$1"           ;; 
-#           *.lzh)       lha x "$1"            ;; 
-           *.xz)        unxz "$1"             ;;
-           *.exe)       cabextract "$1"       ;;
-           *.zip|*.epub|*.cbz)       unzip "$1"            ;; 
-           *.7z|*.z|*.apk|*.deb|*.dmg|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar)        7z x "$1"             ;; 
-           *.zst)       zstd -dc "$1"         ;; 
-           *.zpaq)      zpaq x "$1"           ;;
-           *.zoo)       zoo -extract "$1"     ;; 
-           *)           echo "File not supported : '$1'..." ;; 
-        esac 
-       else 
-        echo "'$1' is not a valid file!" 
-   fi 
- } 
-#alias extract=extractAllTypeFiles
-
-
-if [ $# -eq 0 ]; then
-    echo "No arguments provided"
-    echo "Usage: extract <file1> <file2>"
+if (( $# == 0 )); then
+    printf -- '%s\n' "${0}: No arguments provided" 
+        "Usage: extract <file1> <file2>" >&2
     exit 1
 fi
 
-while [ $# -gt 0 ]
-do
-    extractAllTypeFiles "$1"
-    shift 1
+rc=0
+for fsobj; do
+    xcmd=''
+
+    if [[ ! -r ${fsobj} ]]; then
+        printf -- '%s\n' "${0}: file is unreadable: '${fsobj}'" >&2
+        continue
+    fi
+
+    [[ -e ./"${fsobj#/}" ]] && fsobj="./${fsobj#/}"
+
+    case ${fsobj} in
+        (*.tar.lz)    xcmd=(lzip -d) ;; #tar --lzip -tf $1   ;;
+        (*.tar.7z)
+            7z x -so "${fsobj}" | tar -xf -
+            rc=$(( rc + "${?}" ))
+            continue
+        ;;
+        (*.tar.Z)
+            zcat "${fsobj}" | tar -xvf -
+            rc=$(( rc + "${?}" ))
+            continue
+        ;; 
+        (*.cbt|*.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
+            case "${fsobj}" in
+                (*.cbt|*.tar)       taropts=( xvf ) ;;
+                (*.tar.bz2|*.tbz2)  taropts=( xvjf ) ;; 
+                (*.tar.gz|*.tgz)    taropts=( xvzf ) ;; 
+                (*.tar.lzma)        taropts=( --lzma -xvf ) ;; 
+                (*.tar.xz)          taropts=( -xf ) ;; 
+            esac
+            xcmd=(tar "${taropts[@]}")
+        ;;
+        (*.7z*|*.apk|*.arj|*.cab|*.chm|*.deb|*.dmg|*.iso|*.lzh|*.msi|*.pkg|*.rpm|*.udf|*.wim|*.xar)
+            xcmd=(7z x)
+        ;;
+        (*.ace|*.cba)         xcmd=(unace x) ;;
+        (*.arc|*.ark)         xcmd=(arc e) ;;
+        (*.cbr|*.rar)         xcmd=(unrar x) ;;
+        (*.cbz|*.epub|*.zip)  xcmd=(unzip) ;;
+        (*.cpio)
+            cpio -id < "${fsobj}"
+            rc=$(( rc + "${?}" ))
+            continue
+        ;;
+        (*.cso)
+            ciso 0 "${fsobj}" "${fsobj}".iso
+            extract "${fsobj}".iso
+            rm -rf "${fsobj:?}"; rc=$(( rc + "${?}" ))
+            continue
+        ;;
+        (*.bz2)   xcmd=(bunzip2) ;;
+        (*.exe)   xcmd=(cabextract) ;;
+        (*.gz)    xcmd=(gunzip) ;;
+        (*.jar)   xcmd=(jar xf) ;;
+        (*.lz4)   xcmd=(lz4 -d) ;; 
+        (*.lzma)  xcmd=(unlzma) ;;
+        (*.xz)    xcmd=(unxz) ;;
+        (*.Z|*.z) xcmd=(uncompress) ;;
+        (*.zpaq)  xcmd=(zpaq x) ;;
+        (*.zoo)   xcmd=(zoo -extract) ;; 
+        (*.zst)   xcmd=(zstd -dc) ;; 
+        (*)
+            printf -- '%s\n' "${0}: unrecognized file extension: '${fsobj}'" >&2
+            continue
+        ;;
+    esac
+
+    command "${xcmd[@]}" "${fsobj}"
+    rc=$(( rc + "${?}" ))
 done
+(( rc > 0 )) && exit "${rc}"
+exit 0
